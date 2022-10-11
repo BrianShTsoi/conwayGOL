78828d3190744d917709d8a84c21dabf62dbb681
BrianShTsoi <brian.s.tsoi@gmail.com>
2022-10-11 09:22:34 -0400

    Add cell painting mechanism

--- javascript.js
+++ javascript.js
@@ -3,21 +3,35 @@ create_cells();
 function create_cells() {
     const container = document.querySelector(".container");
     const CELL_LEN = 20;
-    const row_num = Math.round(container.clientHeight / (CELL_LEN + 2));
-    const col_num = Math.round(container.clientWidth / (CELL_LEN + 2));
+    const CELL_MARGIN = 2;
+    const ROW_NUM = 90;
+    const COL_NUM = 180;
 
     let cells = [];
-    for (let i = 0; i < row_num; i++) {
+    for (let i = 0; i < ROW_NUM; i++) {
         cells[i] = [];
         let row = document.createElement("div");
         row.classList.toggle("row");
         container.appendChild(row);
 
-        for (let j = 0; j < col_num; j++) {
+        for (let j = 0; j < COL_NUM; j++) {
             cells[i][j] = document.createElement("div");
             cells[i][j].classList.toggle("cell");
-            cells[i][j].style.height = `${CELL_LEN}px`
-            cells[i][j].style.width = `${CELL_LEN}px`
+            cells[i][j].style.height = `${CELL_LEN}px`;
+            cells[i][j].style.width = `${CELL_LEN}px`;
+            cells[i][j].style.margin = `${CELL_MARGIN / 2}px`;
+            cells[i][j].addEventListener("mousedown", e => {
+                e.target.classList.toggle("alive");
+            })
+            cells[i][j].addEventListener("mouseenter", e => {
+                if (e.buttons === 1) e.target.classList.add("alive");
+            })
+
+            // Below are code for displaying cell coordinates
+            // cells[i][j].textContent = `${i}, ${j}`;
+            // cells[i][j].style.color = "black";
+            // cells[i][j].style.fontSize = "xx-small";
+
             row.appendChild(cells[i][j]);
         }
     }
